f453265454a09275311c5453c710824b1acb40ec
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-19 15:50:14 +0200

    16. React Router - Initial Setup
    -u komponenti App.jsx dodana logika za React Router Dom
    -manje izmjene u documentation.txt fajlu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-const App = () => {
-  return (
-    <h1>Jobify App</h1>
-  )
-}
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-export default App
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>home</h1>,
+  },
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h2>about page</h2>
+      </div>
+    ),
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
