82f8718c73f9e32f07c4b8421d9c4586341ce1cb
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-20 17:02:14 +0200

    38. React Icons
    -u komponentu Navbar.jsx dodana ikonica FaHome
    -manje izmjene u documentation.txt fajlu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import Wrapper from "../assets/wrappers/Navbar"
+import { FaHome } from "react-icons/fa"
 
 const Navbar = () => {
   return (
     <Wrapper>
        Navbar
+       <FaHome />
     </Wrapper>
   )
 }
