df74b5af5a409e67424eae3c1d6033f51d251c8c
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-20 17:38:44 +0200

    42. Sidebar Structure
    -u komponentu SmallSidebar.jsx importujemo niz links, komponentu NavLink, ikonicu FaTimes
    -manje izmjene u documentation.txt fajlu

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,5 +1,10 @@
-import Wrapper from "../assets/wrappers/SmallSidebar"
-import { useDashboardContext } from "../pages/DashboardLayout"
+import { NavLink } from 'react-router-dom'
+import { useDashboardContext } from '../pages/DashboardLayout'
+import { FaTimes } from 'react-icons/fa'
+
+import Wrapper from '../assets/wrappers/SmallSidebar'
+import Logo from './Logo'
+import links from '../utils/links'
 
 const SmallSidebar = () => {
   const data = useDashboardContext()
@@ -7,7 +12,32 @@ const SmallSidebar = () => {
 
   return (
     <Wrapper>
-        SmallSidebar
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button type='button' className='close-btn'>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            {links.map((link) => {
+              const { text, path, icon } = link;
+
+              return (
+                <NavLink
+                  key={text}
+                  to={path}
+                  className='nav-link'
+                >
+                  <span className='icon'>{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
