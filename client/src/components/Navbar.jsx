0fd49daffe1487316103dcbb300868bc21e1cd9e
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-20 17:08:18 +0200

    39. Navbar Structure
    -u komponentu Navbar.jsx dodana logika za Context
    -manje izmjene u documentation.txt fajlu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,25 @@
-import Wrapper from "../assets/wrappers/Navbar"
-import { FaHome } from "react-icons/fa"
+import { FaAlignLeft } from 'react-icons/fa';
+import { useDashboardContext } from '../pages/DashboardLayout';
+import Logo from './Logo';
+import Wrapper from '../assets/wrappers/Navbar';
 
 const Navbar = () => {
+  const { toggleSidebar } = useDashboardContext();
+
   return (
     <Wrapper>
-       Navbar
-       <FaHome />
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn' onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className='logo-text'>dashboard</h4>
+        </div>
+        <div className='btn-container'>toggle/logout</div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
