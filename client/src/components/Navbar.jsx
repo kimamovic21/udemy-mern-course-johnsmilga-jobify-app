862e71eb8e069ea80f2909c68d87f0bf061ac447
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-21 17:36:16 +0200

    50. Theme Toggle
    -u folderu components kreirana komponenta ThemeToggle.jsx
    -u komponentu Navbar.jsx dodana komponenta ThemeToggle.jsx
    -manje izmjene u documentation.txt fajlu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { useDashboardContext } from '../pages/DashboardLayout'
 import Logo from './Logo'
 import Wrapper from '../assets/wrappers/Navbar'
 import LogoutContainer from './LogoutContainer'
+import ThemeToggle from './ThemeToggle'
 
 const Navbar = () => {
   const { toggleSidebar } = useDashboardContext()
@@ -18,6 +19,7 @@ const Navbar = () => {
           <h4 className='logo-text'>dashboard</h4>
         </div>
         <div className='btn-container'>
+          <ThemeToggle />
           <LogoutContainer />
         </div>
       </div>
