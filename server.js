992f685a0693469071d5fb596273dc6815f48aba
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-22 15:14:27 +0200

    60. JSON Middleware
    -u Postman-u testiramo Home rutu sa metodom POST
    -manje izmjene u server.js fajlu
    -manje izmjene u documentation.txt fajlu

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+    res.json({ message: 'Data received', data: req.body });
 });
 
 app.listen(5100, () => {
