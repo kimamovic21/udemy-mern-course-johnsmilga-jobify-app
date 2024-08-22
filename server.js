290fd26d7f0ac6d94e091fd3999ab5b4c48321e5
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-22 17:35:09 +0200

    67. Delete Job
    -u Postman-u testirana ruta Delete Job sa metodom DELETE
    -u server.js fajlu dodana metoda app.delete() i logika za brisanje posla
    -manje izmjene u documentation.txt fajlu

--- server.js
+++ server.js
@@ -55,7 +55,6 @@ app.get('/api/v1/jobs/:id', (req, res) => {
 });
 
 // EDIT JOB
-
 app.patch('/api/v1/jobs/:id', (req, res) => {
     const { company, position } = req.body;
     if (!company || !position) {
@@ -70,6 +69,18 @@ app.patch('/api/v1/jobs/:id', (req, res) => {
     job.company = company;
     job.position = position;
     res.status(200).json({ msg: 'job modified', job });
+});
+
+app.delete('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id);
+    if (!job) {
+      return res.status(404).json({ msg: `no job with id ${id}` });
+    };
+    const newJobs = jobs.filter((job) => job.id !== id);
+    jobs = newJobs;
+  
+    res.status(200).json({ msg: 'job deleted' });
 });
 
 const port = process.env.PORT || 5100;
