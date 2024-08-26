ca1f4d7fcc0c09563fc338d7d3169aecdff82dfb
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-26 17:51:06 +0200

    91. User Controller and Router
    -u folderu controllers kreiran authController.js fajl
    -u folderu routes kreiran authRouter.js fajl
    -u server.js fajlu kreirana metoda app.use() za rute iz authRouter.js fajla
    -u Postman-u testirane rute register i login
    -manje izmjene u documentation.txt fajlu

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 
+// routers
 import jobRouter from './routes/jobRouter.js';
+import authRouter from './routes/authRouter.js';
+
+// middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 
 const app = express();
@@ -20,6 +24,7 @@ app.get('/', (req, res) => {
 });
   
 app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/auth', authRouter);
 
 app.use('*', (req, res) => {
     res.status(404).json({ msg: 'not found' });
