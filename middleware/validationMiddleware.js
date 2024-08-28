9a19c0c4cc6f61d86786653e3fa96bf4504014eb
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-28 17:30:03 +0200

    106. Validate Owner
    -u validationMiddleware.js fajlu, u funkciju validateIdParam dodana logika za validaciju korisnika koji je kreirao posao
    -u Postman-u testirana ruta za pretrazivanje posla po id-u
    -manje izmjene u documentation.txt fajlu

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -3,7 +3,7 @@ import Job from '../models/JobModel.js';
 import User from '../models/UserModel.js';
 
 import { body, validationResult, param } from 'express-validator';
-import { BadRequestError, NotFoundError } from '../errors/customErrors.js';
+import { BadRequestError, NotFoundError, UnauthorizedError } from '../errors/customErrors.js';
 import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
 
 const withValidationErrors = (validateValues) => {
@@ -15,9 +15,12 @@ const withValidationErrors = (validateValues) => {
         const errorMessages = errors.array().map((error) => error.msg);
         if (errorMessages[0].startsWith('no job')) {
           throw new NotFoundError(errorMessages);
-        }
+        };
+        if (errorMessages[0].startsWith('not authorized')) {
+          throw new UnauthorizedError('not authorized to access this route')
+        };
         throw new BadRequestError(errorMessages);
-      }
+      };
       next();
     },
   ];
@@ -32,11 +35,15 @@ export const validateJobInput = withValidationErrors([
 ]);
 
 export const validateIdParam = withValidationErrors([
-  param('id').custom(async (value) => {
+  param('id').custom(async (value, { req }) => {
     const isValidId = mongoose.Types.ObjectId.isValid(value);
     if (!isValidId) throw new BadRequestError('invalid MongoDB id');
     const job = await Job.findById(value);
     if (!job) throw new NotFoundError(`no job with id : ${value}`);
+    // console.log(req);
+    const isAdmin = req.user.role === 'admin';
+    const isOwner = req.user.userId === job.createdBy.toString();
+    if (!isAdmin && !isOwner) throw new UnauthorizedError('not authorized to access this route');
   }),
 ]);
 
