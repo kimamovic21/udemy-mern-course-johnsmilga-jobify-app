fae0763be4d9116db1b8e1768fbd83feabac761c
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-28 16:08:56 +0200

    104. Verify JWT
    -manja izmjena u jobController.js fajlu
    -u tokenUtils.js fajlu kreirana funkcija verifyJWT()
    -u authMiddleware.js fajl dodana funkcija verifyJWT
    -u Postman-u testirane rute za prijavu korisnika i dohvacanje svih poslova
    -manje izmjene u documentation.txt fajlu

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,20 @@
-import { UnauthenticatedError } from "../errors/customErrors.js";
+import { UnauthenticatedError } from '../errors/customErrors.js';
+import { verifyJWT } from '../utils/tokenUtils.js';
 
 export const authenticateUser = async (req, res, next) => {
-    const { token } = req.cookies;
-    if (!token) throw new UnauthenticatedError('authentication invalid');
+  const { token } = req.cookies;
+  console.log(token);
+  if (!token) {
+    throw new UnauthenticatedError('authentication invalid');
+  };
+
+  try {
+    // const user = verifyJWT(token);
+    // console.log('user: ', user);
+    const { userId, role } = verifyJWT(token);
+    req.user = { userId, role };
     next();
+  } catch (error) {
+    throw new UnauthenticatedError('authentication invalid');
+  };
 };
