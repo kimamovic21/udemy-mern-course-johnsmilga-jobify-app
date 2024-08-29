9c18267cfef94cd27d4909dcec1a7faa294421bf
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-29 15:49:38 +0200

    108. User Routes
    -u folderu controllers kreiran userController.js fajl
    -u folderu routes kreiran userRouter.js fajl
    -manje izmjene u authMiddleware.js fajlu
    -u server.js fajl dodana funkcija userRouter i kreirana metoda app.use() za istu
    -u Postman-u kreirane i testirane rute Get Current User, Get Application Stats i Update User
    -manje izmjene u documentation.txt fajlu

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,14 +3,12 @@ import { verifyJWT } from '../utils/tokenUtils.js';
 
 export const authenticateUser = async (req, res, next) => {
   const { token } = req.cookies;
-  console.log(token);
+
   if (!token) {
     throw new UnauthenticatedError('authentication invalid');
   };
 
   try {
-    // const user = verifyJWT(token);
-    // console.log('user: ', user);
     const { userId, role } = verifyJWT(token);
     req.user = { userId, role };
     next();
