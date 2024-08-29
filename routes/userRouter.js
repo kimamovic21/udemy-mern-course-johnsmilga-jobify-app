e6fef400cfb3aff4a373fc24cbebabaa5a59494a
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-29 17:14:20 +0200

    110. Update User
    -u userController.js fajlu, u funkciji updateUser() dodana logika za promjenu podataka za korisnika
    -u validationMiddleware.js fajlu kreirana varijabla validateUpdateUserInput
    -u userRouter.js fajl dodana funkcija validateUpdateUserInput
    -manje izmjene u documentation.txt fajlu

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,9 +2,10 @@ import { Router } from 'express';
 const router = Router();
 
 import { getCurrentUser, getApplicationStats, updateUser } from '../controllers/userController.js';
+import { validateUpdateUserInput } from '../middleware/validationMiddleware.js';
 
 router.get('/current-user', getCurrentUser);
 router.get('/admin/app-stats', getApplicationStats);
-router.patch('/update-user', updateUser);
+router.patch('/update-user', validateUpdateUserInput, updateUser);
 
 export default router;
