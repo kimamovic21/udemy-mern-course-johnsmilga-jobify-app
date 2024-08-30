fbb9f7fab428d10f73dc95f8086b1602ee9a9790
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-30 12:05:12 +0200

    112. Proxy Setup
    -u main.jsx fajl dodana metoda fetch()
    -u vite.config.js fajl dodan konfiguracioni objekt server
    -u server.js fajlu kreirana metoda app.get() za test rutu
    -manje izmjene u documentation.txt fajlu

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+fetch('/api/v1/test')
+  .then(res => res.json())
+  .then(data => console.log(data));
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
