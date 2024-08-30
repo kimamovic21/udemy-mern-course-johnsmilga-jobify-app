584e248abd287fe49cb695330aba20c2f57b22f4
kimamovic21 <imamovic.kerim@gmail.com>
2024-08-30 14:55:01 +0200

    116. First Action
    -u komponentu Register dodana komponenta Form i funkcije redirece i useNavigation iz paketa React Router Dom
    -kreirana i eksportovana asinhrona funkcija action
    -u komponentu App.jsx dodana funkcija action kao registerAction
    -manje izmjene u documentation.txt fajlu

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,19 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+
+export const action = async (data) => {
+  console.log(data);
+  return null;
+};
 
 const Register = () => {
   return (
     <Wrapper>
-      <form className='form'>
+      <Form method='post' className='form'>
         <Logo />
         <h4>Register</h4>
-        <FormRow type='text' name='name' />
-        <FormRow type='text' name='lastName' labelText='last name' />
-        <FormRow type='text' name='location' />
-        <FormRow type='email' name='email' />
+        <FormRow type='text' name='name' defaultValue='Kerim'/>
+        <FormRow type='text' name='lastName' defaultValue='Imamovic' labelText='last name' />
+        <FormRow type='text' name='location' defaultValue='Sarajevo'/>
+        <FormRow type='email' name='email' defaultValue='kerim@email.com'/>
 
-        <FormRow type='password' name='password' />
+        <FormRow type='password' name='password' defaultValue='123456'/>
 
         <button type='submit' className='btn btn-block'>
           submit
@@ -24,7 +29,7 @@ const Register = () => {
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
