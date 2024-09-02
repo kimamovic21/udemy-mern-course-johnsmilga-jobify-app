455f7a35e5d9da76b5f44c2d15d98a4696c1cd35
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-02 16:09:13 +0200

    129. All Jobs Structure
    -u folderu components kreirane komponente JobsContainer.jsx i SearchContainer.jsx
    -u index.js fajlu eksportovane kreirane komponente
    -u komponentu App.jsx dodana funkcija loader kao allJobsLoader
    -u komponentu AllJobs.jsx dodane komponente SearchContainer.jsx i JobsContainer.jsx
    -manje izmjene u documentation.txt fajlu

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,6 +1,28 @@
+import { useLoaderData } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { JobsContainer, SearchContainer } from '../components'
+import customFetch from '../utils/customFetch'
+
+export const loader = async ({ request }) => {
+  try {
+    const { data } = await customFetch.get('/jobs')
+    console.log(data)
+    return { data }
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
+}
+
 const AllJobs = () => {
+  const { data } = useLoaderData()
+  console.log(data)
+
   return (
-    <h2>AllJobs</h2>
+    <>
+      <SearchContainer />
+      <JobsContainer />
+    </>
   )
 }
 
