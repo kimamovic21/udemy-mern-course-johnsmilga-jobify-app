a3ab40573b4e0d1ea4ece3dde492fcff08d13f2b
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-02 16:56:42 +0200

    131. Jobs Container JSX
    -u folderu components kreirana komponenta Job.jsx
    -u komponentu JobsContainer.jsx dodana komponenta Job.jsx
    -manje izmjene u documentation.txt fajlu

---
+++ client/src/components/Job.jsx
@@ -0,0 +1,7 @@
+const Job = () => {
+  return (
+    <h3>Job</h3>
+  )
+}
+
+export default Job
--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useAllJobsContext } from '../pages/AllJobs'
+import Job from './Job'
+import Wrapper from '../assets/wrappers/JobsContainer'
 
 const JobsContainer = () => {
+  const { data } = useAllJobsContext()
+  const { jobs } = data
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    )
+  }
+
   return (
-    <div>JobsContainer</div>
+    <Wrapper>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          return (
+            <Job key={job._id} {...job} />
+          )
+        })}
+      </div>
+    </Wrapper>
   )
 }
 
