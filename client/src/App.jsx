446f640d659a54c5eb7cec0352f8a673887de211
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-02 15:01:29 +0200

    127. Create Job Functionality
    -u komponentu AddJob.jsx dodana logika za kreiranje posla
    -u komponentu App.jsx dodana funkcija action kao addJobAction
    -manje izmjene u documentation.txt fajlu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import {
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
 import { loader as dashboardLoader } from './pages/DashboardLayout';
+import { action as addJobAction } from './pages/AddJob';
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true'
@@ -52,6 +53,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <AddJob />,
+            action: addJobAction,
           },
           { 
             path: 'stats', 
--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -5,6 +5,20 @@ import { FormRow, FormRowSelect } from '../components'
 import customFetch from '../utils/customFetch'
 import Wrapper from '../assets/wrappers/DashboardFormPage'
 
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+
+  try {
+    await customFetch.post('/jobs', data)
+    toast.success('Job added successfully')
+    return redirect('all-jobs')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
+}
+
 const AddJob = () => {
   const { user } = useOutletContext()
   const navigation = useNavigation()
