9eaadc3737fbedf7743eb53777ea5e85f28f3e81
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-03 16:21:08 +0200

    142. Admin Page Complete
    -u folderu components kreirana komponenta StatItem.jsx
    -u index.js fajlu eksportovana komponenta StatItem.jsx kao default
    -u komponentu Admin.jsx dodana komponenta StatItem.jsx i logika za prikazivanje ukupnog broja korisnika i poslova
    -manje izmjene u documentation.txt fajlu

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,6 +1,7 @@
 import { FaSuitcaseRolling, FaCalendarCheck } from 'react-icons/fa'
 import { useLoaderData, redirect } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { StatItem } from '../components'
 import customFetch from '../utils/customFetch'
 import Wrapper from '../assets/wrappers/StatsContainer'
 
@@ -20,7 +21,20 @@ const Admin = () => {
 
   return (
     <Wrapper>
-      <h2>admin page</h2>
+      <StatItem
+        title='current users'
+        count={users}
+        color='#e9b949'
+        bcg='#fcefc7'
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatItem
+        title='total jobs'
+        count={jobs}
+        color='#647acb'
+        bcg='#e0e8f9'
+        icon={<FaCalendarCheck />}
+      />
     </Wrapper>
   )
 }
