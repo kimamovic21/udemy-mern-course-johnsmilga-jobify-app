2d85e4652b5c46939eda84c6f9a4742896a0dc59
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-03 14:34:31 +0200

    138. Edit Job JSX
    -u komponenti EditJob.jsx, u jsx-u dodane komponente i HTML elementi za izmjenu podataka o poslu
    -manje izmjene u documentation.txt fajlu

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,6 +1,9 @@
-import { redirect, useParams, useLoaderData } from 'react-router-dom'
+import { Form, useNavigation, redirect, useParams, useLoaderData } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants'
+import { FormRow, FormRowSelect } from '../components'
 import customFetch from '../utils/customFetch'
+import Wrapper from '../assets/wrappers/DashboardFormPage'
 
 export const loader = async ({ params }) => {
   try {
@@ -17,13 +20,58 @@ export const action = async () => {
 }
 
 const EditJob = () => {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
+
   const params = useParams()
   console.log(params)
   const { job } = useLoaderData()
   console.log(job)
 
   return (
-    <h2>EditJob</h2>
+    <Wrapper>
+      <Form method='post' className='form'>
+        <h4 className='form-title'>edit job</h4>
+        <div className='form-center'>
+          <FormRow 
+            type='text' 
+            name='position' 
+            defaultValue={job.position} 
+          />
+          <FormRow 
+            type='text'
+            name='company' 
+            defaultValue={job.company} 
+          />
+          <FormRow
+            type='text'
+            labelText='job location'
+            name='jobLocation'
+            defaultValue={job.jobLocation}
+          />
+
+          <FormRowSelect
+            name='jobStatus'
+            labelText='job status'
+            defaultValue={job.jobStatus}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            name='jobType'
+            labelText='job type'
+            defaultValue={job.jobType}
+            list={Object.values(JOB_TYPE)}
+          />
+          <button
+            type='submit'
+            className='btn btn-block form-btn '
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'submitting...' : 'submit'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
   )
 }
 
