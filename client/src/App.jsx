53534325988334ce6a1db102794ef7914e92a531
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-03 13:57:00 +0200

    136. Route Params
    -u komponenti EditJob.jsx kreirane funkcije action() i loader()
    -u komponentu App.jsx dodane funkcije action kao editJobAction i loader kao editJobLoader
    -manje izmjene u documentation.txt fajlu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,8 @@ import { action as loginAction } from './pages/Login';
 import { loader as dashboardLoader } from './pages/DashboardLayout';
 import { action as addJobAction } from './pages/AddJob';
 import { loader as allJobsLoader } from './pages/AllJobs';
+import { loader as editJobLoader } from './pages/EditJob';
+import { action as editJobAction } from './pages/EditJob';
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true'
@@ -76,6 +78,8 @@ const router = createBrowserRouter([
           {
             path: 'edit-job/:id',
             element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
           },
         ],
       },
--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,4 +1,18 @@
+import { useParams } from 'react-router-dom'
+
+export const loader = async ({ params }) => {
+  console.log(params)
+  return null
+}
+
+export const action = async () => {
+  return null
+}
+
 const EditJob = () => {
+  const params = useParams()
+  console.log(params)
+
   return (
     <h2>EditJob</h2>
   )
