932ec4d103a76ae69ce7449af70fc20cebb8b884
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-03 14:06:24 +0200

    137. Edit Job Loader
    -u komponenti EditJob.jsx, u funkciju loader() dodana logika za dohvacanje posla po id-u
    -manje izmjene u documentation.txt fajlu

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,8 +1,15 @@
-import { useParams } from 'react-router-dom'
+import { redirect, useParams, useLoaderData } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import customFetch from '../utils/customFetch'
 
 export const loader = async ({ params }) => {
-  console.log(params)
-  return null
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`)
+    return data
+  } catch (error) {
+    toast.error(error.response.data.msg)
+    return redirect('/dashboard/all-jobs')
+  }
 }
 
 export const action = async () => {
@@ -12,6 +19,8 @@ export const action = async () => {
 const EditJob = () => {
   const params = useParams()
   console.log(params)
+  const { job } = useLoaderData()
+  console.log(job)
 
   return (
     <h2>EditJob</h2>
