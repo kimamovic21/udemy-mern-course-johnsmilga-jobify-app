f251e7474476af5f7b225df54b4fba96cdde0aba
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-04 17:27:54 +0200

    151. Update User Functionality
    -u userController.js fajlu, u funkciji updateUser() dodana logika za promjenu podataka o prijavljenom korisniku i logika za postavljanje slike na servis Cloudinary
    -manje izmjene u documentation.txt fajlu

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
+import { promises as fs } from 'fs';
+import cloudinary from 'cloudinary';
 import User from '../models/UserModel.js';
 import Job from '../models/JobModel.js';
 
@@ -15,10 +17,21 @@ export const getApplicationStats = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
-    console.log(req.file);
-    const obj = { ...req.body };
-    delete obj.password;
-    console.log(obj);
-    const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
-    res.status(StatusCodes.OK).json({ msg: 'user updated' });
+  const newUser = { ...req.body };
+  delete newUser.password;
+
+  if (req.file) {
+    const response = await cloudinary.v2.uploader.upload(req.file.path);
+    await fs.unlink(req.file.path);
+    newUser.avatar = response.secure_url;
+    newUser.avatarPublicId = response.public_id;
+  };
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
+
+  if (req.file && updatedUser.avatarPublicId) {
+    await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId);
+  };
+  
+  res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
