3c685c84affba08966cde675ae62d2ba3945c80e
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-04 14:22:48 +0200

    148. Profile Action
    -u komponenti Profile.jsx kreirana funkcija action()
    -u komponentu App.jsx dodana funkcija action kao profileAction
    -manje izmjene u documentation.txt fajlu

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,31 @@
 import { Form, useNavigation, useOutletContext } from 'react-router-dom'
 import { FormRow } from '../components'
+import { toast } from 'react-toastify'
+import customFetch from '../utils/customFetch'
 import Wrapper from '../assets/wrappers/DashboardFormPage'
+
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+
+  const file = formData.get('avatar')
+  if (file && file.size > 500000) {
+    toast.error('Image size too large!')
+    return null
+  }
+
+  try {
+    await customFetch.patch('/users/update-user', formData)
+    toast.success('Profile updated successfully!')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+  }
+  return null
+}
 
 const Profile = () => {
   const { user } = useOutletContext()
-  console.log(user)
-  
   const { name, lastName, email, location } = user
+
   const navigation = useNavigation()
   const isSubmitting = navigation.state === 'submitting'
 
