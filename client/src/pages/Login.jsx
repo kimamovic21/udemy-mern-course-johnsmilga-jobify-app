93ccb544e4d6258a2da0fbda411d0316b57c8f72
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-05 12:46:43 +0200

    153. Create Test User
    -u komponentu Login.jsx kreirana funkcija loginDemoUser() za prijavu demo korisnika
    -manje izmjene u documentation.txt fajlu

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { Link, Form, redirect } from 'react-router-dom'
+import { Link, Form, redirect, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { FormRow, Logo, SubmitBtn } from '../components'
 import customFetch from '../utils/customFetch'
@@ -19,17 +19,38 @@ export const action = async ({ request }) => {
 }
 
 const Login = () => {
+  const navigate = useNavigate()
+
+  const loginDemoUser = async () => {
+    const data = {
+      email: 'test@test.com',
+      password: 'secret123',
+    }
+    try {
+      await customFetch.post('/auth/login', data)
+      toast.success('ake a test drive')
+      navigate('/dashboard')
+    } catch (error) {
+      toast.error(error?.response?.data?.msg)
+    }
+  }
+
   return (
     <Wrapper>
       <Form method='post' className='form'>
         <Logo />
         <h4>Login</h4>
+        
         <FormRow type='email' name='email' defaultValue='kerim@email.com' />
         <FormRow type='password' name='password' defaultValue='12345678' />
         
         <SubmitBtn />
 
-        <button type='button' className='btn btn-block'>
+        <button 
+          type='button' 
+          className='btn btn-block' 
+          onClick={loginDemoUser}
+        >
           explore the app
         </button>
 
