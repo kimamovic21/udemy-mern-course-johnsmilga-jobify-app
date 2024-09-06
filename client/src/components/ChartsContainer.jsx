a67c2cf0383afebf0e319079a534c6eeb587b697
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-06 12:29:14 +0200

    162. BarChart Container
    -u komponentu ChartsContainer.jsx dodane komponente AreaChart.jsx i BarChart.jsx
    -manje izmjene u documentation.txt fajlu

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,6 +1,19 @@
-const ChartsContainer = () => {
+import { useState } from 'react'
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import Wrapper from '../assets/wrappers/ChartsContainer'
+
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true)
+
   return (
-    <h2>ChartsContainer</h2>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
   )
 }
 
