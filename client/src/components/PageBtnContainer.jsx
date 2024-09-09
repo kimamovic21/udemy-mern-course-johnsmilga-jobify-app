63a03f17ba5226fafd920ea484ef0be13fcbf145
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-09 15:02:52 +0200

    175. Render Buttons
    -u komponentu PageBtnContainer.jsx dodana logika za prikazivanje button elemenata
    -manje izmjene u documentation.txt fajlu

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,12 +1,33 @@
 import { useAllJobsContext } from "../pages/AllJobs"
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
+import Wrapper from '../assets/wrappers/PageBtnContainer'
 
 const PageBtnContainer = () => {
   const { data: { numOfPages, currentPage } } = useAllJobsContext()
-  console.log('numOfPages: ', numOfPages)
-  console.log('currentPage: ', currentPage)
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1
+  })
+  console.log('pages: ', pages)
 
   return (
-    <h2>PageBtnContainer</h2>
+    <Wrapper>
+        <button className='btn prev-btn'>
+            <HiChevronDoubleLeft />
+        </button>
+        <div className='btn-container'>
+            {pages.map((pageNumber) => (
+                <button
+                    key={pageNumber}
+                    className={`btn page-btn ${pageNumber === currentPage && 'active'}`}
+                >
+                    {pageNumber}
+                </button>
+            ))}
+        </div>
+        <button className='btn next-btn'>
+            <HiChevronDoubleRight />
+        </button>
+    </Wrapper>
   )
 }
 
