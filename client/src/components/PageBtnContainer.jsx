4f405b8482b99e76b58111654c574ed604d21d7e
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-09 15:29:18 +0200

    176. Pagination Logic - First Approach
    -u komponentu PageBtnContainer.jsx dodana logika za paginaciju u button elemente
    -manje izmjene u documentation.txt fajlu

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,3 +1,4 @@
+import { useLocation, useNavigate } from 'react-router-dom'
 import { useAllJobsContext } from "../pages/AllJobs"
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi'
 import Wrapper from '../assets/wrappers/PageBtnContainer'
@@ -7,24 +8,49 @@ const PageBtnContainer = () => {
   const pages = Array.from({ length: numOfPages }, (_, index) => {
     return index + 1
   })
-  console.log('pages: ', pages)
+
+  const { search, pathname } = useLocation()
+  const navigate = useNavigate()
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search)
+    searchParams.set('page', pageNumber)
+    navigate(`${pathname}?${searchParams.toString()}`)
+  }
 
   return (
     <Wrapper>
-        <button className='btn prev-btn'>
+        <button 
+            className='btn prev-btn'
+            onClick={() => {
+                let prevPage = currentPage - 1
+                if (prevPage < 1) prevPage = numOfPages
+                handlePageChange(prevPage)
+            }}
+        >
             <HiChevronDoubleLeft />
+            prev
         </button>
         <div className='btn-container'>
             {pages.map((pageNumber) => (
                 <button
                     key={pageNumber}
                     className={`btn page-btn ${pageNumber === currentPage && 'active'}`}
+                    onClick={() => handlePageChange(pageNumber)}
                 >
                     {pageNumber}
                 </button>
             ))}
         </div>
-        <button className='btn next-btn'>
+        <button 
+            className='btn next-btn'
+            onClick={() => {
+                let nextPage = currentPage + 1
+                if (nextPage > numOfPages) nextPage = 1
+                handlePageChange(nextPage)
+            }}
+        >
+            next
             <HiChevronDoubleRight />
         </button>
     </Wrapper>
