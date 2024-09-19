dd4c331676b4fa3c9a0572ed135b41ec5cb20e55
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-19 15:10:00 +0200

    187. First Query with React Query
    -u komponentu Stats.jsx dodana funkcija useQuery
    -u main.jsx fajlu izbrisana komponenta StrictMode
    -manje izmjene u documentation.txt fajlu

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -6,8 +5,8 @@ import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <App />
     <ToastContainer />
-  </StrictMode>
+  </>
 )
--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,16 +1,25 @@
-import { useLoaderData } from 'react-router-dom'
+// import { useLoaderData } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
 import { ChartsContainer, StatsContainer } from '../components'
 import customFetch from '../utils/customFetch'
 
 export const loader = async () => {
-  // const { data } = await customFetch.get('/jobs/statss')
-    const { data } = await customFetch.get('/jobs/stats')
-    return data
+  return null
 }
 
 const Stats = () => {
-  const { defaultStats, monthlyApplications } = useLoaderData()
-  
+  // const { defaultStats, monthlyApplications } = useLoaderData()
+ 
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['stats'],
+    queryFn: () => customFetch('/jobs/stats')
+  })
+
+  if (isLoading) return <h4>Loading...</h4>
+  if (isError) return <h4>There was an error...</h4>
+
+  const { defaultStats, monthlyApplications } = data.data
+ 
   return (
     <>
       <StatsContainer defaultStats={defaultStats} />
